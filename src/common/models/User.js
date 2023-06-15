7ec3be02f3c59ae2e9259df24f583fa851060d38
Gavin Shelley <gavinshelley10@gmail.com>
2023-06-15 16:38:44 +0100

    Added models for user and role

---
+++ src/common/models/User.js
@@ -0,0 +1,9 @@
+export default class User{
+    constructor(username, password, role, token, id){
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.token = token;
+        this.id = id;
+    }
+}
---
+++ src/components/register/Register.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useState } from 'react'
+
+const Register = () => {
+
+    const [user, setUser] = useState(new User('', ', '))
+
+  return (
+    <div>Register</div>
+  )
+}
+
+export default Register
