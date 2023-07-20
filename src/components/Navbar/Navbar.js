4bc208ecaaa06196edab142128ebe992ee7d224f
Gavin Shelley <gavinshelley10@gmail.com>
2023-07-20 16:52:04 +0100

    Added dropdown working, issue when logout the currentUser is null

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -47,13 +47,13 @@ const Navbar = ({links}) => {
                 <div className="dropdown">
                     <a className="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
                        data-bs-toggle="dropdown" aria-expanded="false">
-                        {currentUser.username}
+                        {/*{currentUser.username} todo-> This causes an error when you logout, {currentUser is NULL} */}
                     </a>
 
                     <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                        <li><a className="dropdown-item" href="#">Action</a></li>
-                        <li><a className="dropdown-item" href="#">Another action</a></li>
-                        <li><a className="dropdown-item" href="#">Something else here</a></li>
+                        <li><NavLink to="/profile" className="dropdown-item">Profile</NavLink></li>
+                        <li><NavLink to="/settings" className="dropdown-item">Settings</NavLink></li>
+                        <li><NavLink to="/logout" className="dropdown-item" onClick={() => logout()}>Logout</NavLink></li>
                     </ul>
                 </div>
 
